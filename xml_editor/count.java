cf9efd2c3c0c23b6fd6f849f3d28f20cf5a674ea
FaresWafy2000 <wafyfares5@gmail.com>
2021-12-12 21:05:07 +0200

    conversion from xml to json

--- xml_editor/count.java
+++ xml_editor/count.java
@@ -1,8 +1,17 @@
-
-package xml_editor;
-
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+// pass by ref
 public class count 
 {
+   
+   public  int counter ;
     
+   count (int a )
+   {
+      counter = a ; 
+   }
 }
