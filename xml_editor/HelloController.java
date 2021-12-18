b24326b2e9b3366f74d32e35fe16d7ad09b87f0a
FaresWafy2000 <wafyfares5@gmail.com>
2021-12-18 21:02:31 +0200

    Create HelloController.java

---
+++ xml_editor/HelloController.java
@@ -0,0 +1,167 @@
+package com.example.guil;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import java.util.Scanner ;
+
+import java.util.ArrayList;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+     static String mystring ;
+    static String st ,formateed  ;
+    StringBuffer str = new  StringBuffer() ;
+    ArrayList<String> fixError2 = new ArrayList<String>();
+    ArrayList<Integer> lineOfError2= new ArrayList<Integer>();
+
+    @FXML
+    private TextArea txt1 ;
+    @FXML
+    private TextArea txt2 ;
+
+    @FXML
+    protected void correctbutton()
+    {
+        Boolean flag = false ;
+        txt2.clear();
+        for (int i = 0; i < lineOfError2.size(); i ++)
+        {
+            if (lineOfError2.get(i) == 1)
+            {
+                flag = true ;
+                break ;
+            }
+        }
+        if(flag)
+        {
+            txt2.appendText(correction.print(fixError2));
+            txt2.appendText("\n"+"press format  then ");
+
+        }
+        else
+        {
+            txt2.appendText("No need to be corrected" );
+        }
+
+
+
+
+    }
+
+    @FXML
+    protected void checkbutton()
+    {
+        txt2.clear();
+        File file = new File(Reader.filepath);
+        Scanner scan = null;
+        try {
+            scan = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> line = new ArrayList<String>();
+        ArrayList<Integer> lineOfError = new ArrayList<Integer>();
+        ArrayList<String> fixError = new ArrayList<String>();
+
+        while(scan.hasNextLine())
+        {
+            line.add(scan.nextLine()) ;
+        }
+        try
+        {
+            String a ;
+          a=  correction.check_error(line, lineOfError, fixError);
+            fixError2 =  fixError;
+            lineOfError2= lineOfError;
+            txt2.appendText(a);
+        }
+        catch(Exception EmptyStackException)
+        {
+            txt2.appendText("Wrong XML file , You have a Closing Tag without Opening it" );
+            txt2.appendText("Please Fix it, then Try again");
+
+        }
+
+
+    }
+
+
+
+
+    @FXML
+    protected void brwosebutton()
+    {
+        String f ;
+        f=Reader.Read();
+        txt1.appendText(f);
+
+
+    }
+    @FXML
+    protected void Compressbutton()
+    {
+        txt2.clear();
+        String a ;
+       a= Compress.compresscaller(formateed);
+        txt2.appendText(a);
+
+
+    }
+    @FXML
+    protected void Decompressbutton()
+    {
+        txt2.clear();
+        String a ;
+        a= Decompress.decompresscaller();
+        txt2.appendText(a);
+
+    }
+    @FXML
+    protected void Minfiybutton()
+    {
+        txt2.clear();
+         String b ;
+         b= Minify.minifycaller(formateed);
+        txt2.appendText(b);
+    }
+    @FXML
+    protected void Fomratbutton()
+    {
+        txt2.clear();
+        st  =correction.listtostring(fixError2);
+
+        int a[]=new int[st.length()+1] ;
+        ArrayList<String> output = new ArrayList<String>();
+        Format myformat=new Format();
+        myformat.parsing_xml_with_array(st,output,a);
+        myformat.Check_ParsingArray(a,output);
+        StringBuilder output2= new StringBuilder();
+        myformat.pretiffy(a,output,output2);
+        String output3=output2.toString();
+        formateed = output3 ;
+        txt2.appendText(String.valueOf(output3));
+    }
+
+    @FXML
+    protected void convertbutton()
+    {
+        txt2.clear();
+        ArrayList<String> xmllist = new ArrayList<>();
+        Node root = new Node();
+        String temp ;
+        temp = Minify.minifycaller(st);
+        Node.parsingtoarraylist(temp, xmllist);
+        root.setNameOfTag(xmllist.get(0));
+        count i=new count(1);
+        tree.filltree(root, xmllist, i);
+        xml2json.convert(root,str);
+        txt2.appendText(String.valueOf(str));
+
+
+    }
+}
